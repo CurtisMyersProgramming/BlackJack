c675c4021942965ea7a65a1945ac0094e1eec8be
Curtis Myers <curtismyers25@gmail.com>
2020-08-12 16:09:52 +0100

    Added content to Deck class - Arraylist for cards - constructor - Create deck method

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Deck {
+    //Arraylist top hold the card objects to form a deck
+    private ArrayList<Card> Deck;
+
+    public Deck() {
+        this.Deck = new ArrayList<>();
+    }
+
+    public void createFullDeck(){
+        for(Suits cardSuit : Suits.values()){ // iterate over each suit
+            for(Values value : Values.values()){ // iterate over each value for each suit
+                Deck.add(new Card(cardSuit,value)); // add to arraylist new card of iterations
+            }
+
+        }
+    } //method to create full deck of cards
 }
