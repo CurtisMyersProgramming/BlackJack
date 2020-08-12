e46bf1d8d5fadb9ade39222a26c2f07514e3c0cb
Curtis Myers <curtismyers25@gmail.com>
2020-08-12 15:04:39 +0100

    Added content to Card class - variables (suit, card enums) - constructor - get and toString method

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,19 @@
 public class Card {
+
+    // values that a card will hold
+    private Suits suit;
+    private Values value;
+    //constructor of card
+    public Card(Suits suit, Values value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public Values getValue() {
+        return value;
+    }
+
+    public String toString(){
+        return this.value + " " +this.suit;
+    }
 }
